d67eb4832e77a04131ab1153ad973866932c9291
pushkarpawar15 <pushkarpawar2002@gmail.com>
2023-11-26 23:37:41 +0530

    "update"

--- src/BubbleSort.js
+++ src/BubbleSort.js
@@ -1,30 +1,3 @@
-export function bubbleSort(items) {
-  const length = items.length;
-  let noSwaps;
-
-  for (let i = length; i > 0; i--) {
-    // flag for optimization
-    noSwaps = true;
-    // Number of passes
-    for (let j = 0; j < i - 1; j++) {
-      // Compare the adjacent positions
-      if (items[j] > items[j + 1]) {
-        // Swap the numbers
-        [items[j], items[j + 1]] = [items[j + 1], items[j]];
-        noSwaps = false;
-      }
-    }
-    if (noSwaps) {
-      break;
-    }
-  }
-
-  return items;
-}
-
-/**
- * Using a while loop and a for loop.
- */
 export function alternativeBubbleSort(arr) {
   let swapped = true;
 
--- test/BubbleSort.test.js
+++ test/BubbleSort.test.js
@@ -1,24 +1,5 @@
-import { alternativeBubbleSort, bubbleSort } from "../src/BubbleSort.js";
+import { alternativeBubbleSort } from "../src/BubbleSort.js";
 import { expect } from "chai";
-
-describe("bubbleSort", () => {
-  it("should sort arrays correctly", () => {
-    expect(bubbleSort([5, 4, 1, 2, 3])).to.eql([1, 2, 3, 4, 5]);
-    expect(bubbleSort([])).to.eql([]);
-    expect(bubbleSort([1, 2, 3])).to.eql([1, 2, 3]);
-    expect(bubbleSort([5, 6, 7, 8, 1, 2, 12, 14])).to.eql([
-      1, 2, 5, 6, 7, 8, 12, 14,
-    ]);
-    expect(bubbleSort([5, 6, 7, 8, 9, 4])).to.eql([4, 5, 6, 7, 8, 9]);
-    expect(bubbleSort([20, 30, 40])).to.eql([20, 30, 40]);
-    expect(bubbleSort([2, 1, 3])).to.eql([1, 2, 3]);
-    expect(bubbleSort([10, 15, 16, 100])).to.eql([10, 15, 16, 100]);
-    expect(bubbleSort([10, 9, 11])).to.eql([9, 10, 11]);
-    expect(bubbleSort([10, 9, 12])).to.eql([9, 10, 12]);
-    expect(bubbleSort([3, 2, 1])).to.eql([1, 2, 3]);
-    expect(bubbleSort([10, 9, 8])).to.eql([8, 9, 10]);
-  });
-});
 
 describe("alternativeBubbleSort", () => {
   it("should sort arrays correctly", () => {
