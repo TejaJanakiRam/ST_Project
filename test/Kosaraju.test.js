c0806d23f0a6bcc94472ef87e3c3aac358999980
TejaJanakiRam <dnt.janakiram@gmail.com>
2023-11-26 18:05:47 +0530

    teja commit

--- test/Kosaraju.test.js
+++ test/Kosaraju.test.js
@@ -1,30 +1,33 @@
-import { kosaraju } from '../Kosaraju.js'
+import { expect } from "chai";
+import { kosaraju } from "../src/Kosaraju";
 
-test('Test Case 1', () => {
-  const graph = [
-    [1, 2],
-    [2, 3],
-    [3, 1],
-    [2, 4],
-    [4, 5],
-    [5, 6],
-    [6, 4]
-  ]
-  const stronglyConnectedComponents = kosaraju(graph)
-  expect(stronglyConnectedComponents).toStrictEqual([
-    [1, 3, 2],
-    [4, 6, 5]
-  ])
-})
+describe("Kosaraju Function", () => {
+  it("Test Case 1", () => {
+    const graph = [
+      [1, 2],
+      [2, 3],
+      [3, 1],
+      [2, 4],
+      [4, 5],
+      [5, 6],
+      [6, 4],
+    ];
+    const stronglyConnectedComponents = kosaraju(graph);
+    expect(stronglyConnectedComponents).to.deep.equal([
+      [1, 3, 2],
+      [4, 6, 5],
+    ]);
+  });
 
-test('Test Case 2', () => {
-  const graph = [
-    [1, 2],
-    [2, 3],
-    [3, 1],
-    [2, 4],
-    [4, 5]
-  ]
-  const stronglyConnectedComponents = kosaraju(graph)
-  expect(stronglyConnectedComponents).toStrictEqual([[1, 3, 2], [4], [5]])
-})
+  it("Test Case 2", () => {
+    const graph = [
+      [1, 2],
+      [2, 3],
+      [3, 1],
+      [2, 4],
+      [4, 5],
+    ];
+    const stronglyConnectedComponents = kosaraju(graph);
+    expect(stronglyConnectedComponents).to.deep.equal([[1, 3, 2], [4], [5]]);
+  });
+});
